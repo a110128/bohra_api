ad21cffed339d431ef396434e3990dc8b0a5a79f
a110128 <adnan.mustafa.sarkar@icloud.com>
2025-12-23 15:31:01 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,14 @@
-print('placeholder')
+import sqlite3
+import os
+
+# Path to the SQLite file inside the data folder
+DB_PATH = os.path.join("data", "Bohra_Calendar_5300AH.sqlite")
+
+def get_connection():
+    """
+    Creates a new connection to the SQLite database.
+    Row factory returns dict-like rows.
+    """
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
